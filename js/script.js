aaf106b956d6ba18ddd549c9fa7b1da2df634105
Villy Rosa <villyrosa@hotmail.com>
2022-06-01 16:26:09 -0300

    Criação do menu e pontuação

--- js/script.js
+++ js/script.js
@@ -1,60 +1,121 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
-const clods = document.querySelector('.clouds');
-const soundJump = new Audio('sounds/jump.wav')
-const soundGameOver = new Audio('sounds/game-over.wav')
-const soundGame = new Audio('sounds/game-music.wav')
+const menu = document.querySelector('.game-board');
+var bestScore = "000000";
+var nowScore = "000000";
 
-const jump = () => {
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
+function start() {
+    menu.innerHTML = `<img src="images/clouds.png" class="clouds">     
+                      <img src="images/pipe.png" class="pipe">
+                      <img src="images/mario.gif" class="mario">
+                      <p class="score">HI ${bestScore} - ${nowScore}</p>`;
 
-    if(marioPosition == 0) {
-        soundJump.play();
-        mario.classList.add('jump');
-
-        setTimeout(() => {
-            mario.classList.remove('jump');
-        }, 500)
-    }
-   
-}
-
-const loop = setInterval(() => {
-
-    soundGame.play();
+    const mario = document.querySelector('img.mario');
+    const pipe = document.querySelector('.pipe');
+    const clods = document.querySelector('.clouds'); 
+    const score = document.querySelector('.score'); 
+    const soundJump = new Audio('sounds/jump.wav')
+    const soundGameOver = new Audio('sounds/game-over.wav')
+    const soundGame = new Audio('sounds/game-music.wav')
     
-    const pipePosition = pipe.offsetLeft;
-    const clodsPosition = clods.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
-
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
-        soundGame.pause();
-        soundGameOver.play();
-
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
-
-        mario.style.animation = 'none';
-        mario.style.animation = 'game-over 5s';
-        mario.style.bottom = `${marioPosition}px`;
-        mario.style.position = `fixed`;
-
-        clods.style.animation = 'none';
-        clods.style.left = `${clodsPosition}px`;
-
-        mario.src = 'images/game-over.png';
-        mario.style.width = '75px';
-        mario.style.marginLeft = '50px';
-
-        window.document.removeEventListener('keydown', jump);
-        window.document.removeEventListener('touchstart', jump);
-
-        clearInterval(loop);
-
+    const jump = () => {
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
+                    
+            if(marioPosition == 0) {
+                soundJump.play();
+                mario.classList.add('jump');
+                    
+                setTimeout(() => {
+                     mario.classList.remove('jump');
+                }, 500)
+        }
+                       
     }
 
-}, 10)
+    const loop = setInterval(() => {
 
-window.document.addEventListener('keydown', jump);
-window.document.addEventListener('touchstart', jump);
+        soundGame.play();
+        score.innerHTML = `HI ${bestScore} - ${nowScore}`
+        
+        const pipePosition = pipe.offsetLeft;
+        const clodsPosition = clods.offsetLeft;
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
+
+        if (nowScore < 999999) {
+            nowScore++;
+        }
+        
+        if (bestScore < nowScore) {
+            bestScore = nowScore;
+            if (bestScore < 10) {
+                bestScore = "00000" + nowScore;
+            } else if (bestScore < 100) {
+                bestScore = "0000" + nowScore;
+            } else if (bestScore < 1000) {
+                bestScore = "000" + nowScore;
+            } else if (bestScore < 10000) {
+                bestScore = "00" + nowScore;
+            } else if (bestScore < 100000) {
+                bestScore = "0" + nowScore;
+            } else if (bestScore > 1000000) {
+                bestScore = "999999";
+            }
+        }
+
+        if (nowScore < 10) {
+            nowScore = "00000" + nowScore;
+        } else if (nowScore < 100) {
+            nowScore = "0000" + nowScore;
+        } else if (nowScore < 1000) {
+            nowScore = "000" + nowScore;
+        } else if (nowScore < 10000) {
+            nowScore = "00" + nowScore;
+        } else if (nowScore < 100000) {
+            nowScore = "0" + nowScore;
+        } else if (nowScore > 1000000) {
+            nowScore = "999999";
+        }
+
+        if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    
+            soundGame.pause();
+            soundGameOver.play();
+    
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
+    
+            mario.style.animation = 'none';
+            mario.style.animation = 'game-over 5s';
+            mario.style.bottom = `${marioPosition}px`;
+            mario.style.position = `fixed`;
+    
+            clods.style.animation = 'none';
+            clods.style.left = `${clodsPosition}px`;
+    
+            mario.src = 'images/game-over.png';
+            mario.style.width = '75px';
+            mario.style.marginLeft = '50px';
+    
+            window.document.removeEventListener('keydown', jump);
+            window.document.removeEventListener('touchstart', jump);
+
+            nowScore = "000000";
+
+            setTimeout(() => {
+                menu.innerHTML = `<img src="images/clouds.png" class="clouds">     
+                                  <img src="images/pipe.png" class="pipe">
+                                  <p class="score">HI ${bestScore} - ${nowScore}</p>
+                                  
+                                  <div class="menu">
+                                    <img src="images/logo.png" alt="">
+                                    <div class="btn" onclick="start()"><h1>Start</h1></div>
+                                    <a href="https://github.com/VillyRosa" target="_blank"><p>&copy;VillyRosa</p></a>
+                                  </div>`          
+           }, 3000)
+            
+           clearInterval(loop);
+        }
+    
+    }, 10)
+
+    window.document.addEventListener('keydown', jump);
+    window.document.addEventListener('touchstart', jump);
+}
